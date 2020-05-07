6fbd5a6266e693d9943e7a5ec7a4313b5b67f85d
Ezequiel Portela <eportelab@gmail.com>
2020-05-07 09:56:42 -0300

    get encrypted env

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,17 @@
+const _ = require('lodash');
+const SimpleCrypto = require('simple-crypto-js').default;
+
 const getEnv = (name) => process.env[name];
 
-module.exports = {getEnv};
+const getEnvParam = (name, defaultValue, keyName = 'API_KEY') => {
+  const env = getEnv(name);
+  if (!_.isEmpty(env)) return env;
+
+  const key = getEnv(keyName);
+  if (_.isEmpty(key)) return null;
+
+  const simpleCrypto = new SimpleCrypto(key);
+  return simpleCrypto.decrypt(defaultValue);
+};
+
+module.exports = {getEnv, getEnvParam};
